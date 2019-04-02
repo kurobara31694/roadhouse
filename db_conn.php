32d581740e80f2445b53639bbfd1afc3aa8144fe
ash751 <47225729+ash751@users.noreply.github.com>
2019-04-02 15:40:47 -0500

    k

---
+++ db_conn.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+
+$username = "user2";
+
+$password = "Hershey@2018";
+
+$db = "test2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Yeet";
+// 
+
+?>
