a9cf7ee0ae9ec213fd5cbc604e8e8238de4e6eb9
nabilahsan <nabilahsan95@gmail.com>
2019-04-17 00:05:42 -0500

    printing username from log table
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- wel.php
+++ wel.php
@@ -1,81 +1,81 @@
-<?php 
+<?php
 include_once('db_conn.php');
 ?>
 <!DOCTYPE html>
 <!-- Main Sign In Page -->
 <html lang="en">
+
 <head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Our stylesheet -->
   <link rel="stylesheet" href="resources/style.css">
-  
-  <title>HOME PAGE</title> 
-  
+
+  <title>HOME PAGE</title>
+
 </head>
 
 <body>
-  <div class="container" id="main">
 
-    <h1>______________________ </h1>
-    <h2>POTATOES</h2>
+  <main class="container">
 
-    <div class="container" id="login-section">
-      <form method="post" action="login.php" >
+    <?php
 
-          <div class="input-group">
-            <label></label>
-            <p>What a wonderful page, look at all the lack of CONTENT!!!!! </p>
-          </div>
-     </form>
+    echo "<h1> User ".$_SESSION['user_name']. " logged in.\r\n </h1>";
+    $uname = $_SESSION['user_name'];
+    $fuel_userId = $_SESSION['user_id'];
 
-     <div class="container">
 
-     <?php 
-  
-  echo $_SESSION['user_name'];
-  echo nl2br(", welcome.\r\n");
-  $uname=$_SESSION['user_name'];
- 
-  
-  $query="SELECT * FROM log
-  WHERE user_name='$uname' ";
-  echo "---Current User Information--- ";
-  if ($result = $dbconn->query($query)) {
- 
-    while ($row = $result->fetch_assoc()) {
-      $field1name = $row["full_name"];
-      $field2name = $row["city"];
-      $field3name = $row["state"];
-      $field4name = $row["zip"];
+    $query1 = "SELECT * FROM log WHERE user_name='$uname'";
+    // $query2 = "SELECT user_id FROM fuelcalc where user_id ='$fuel_userId'";
+    
+    echo "---Current User Information--- ";
 
-      echo nl2br("\r\nName: ");
-      echo $field1name;
-      
-      echo nl2br("\r\nState: ");
-      echo $field3name;
-      echo nl2br("\r\nCity: ");
-      echo $field2name;
-      echo nl2br("\r\nZip: ");
-      echo $field4name;
+    // if ($result2 = $dbconn->query($query2)){
+    //   while ($row = $result2->fetch_assoc()){
+    //     $userID = $row["user_id"];
+    //     echo "<h5> User id from fuelcalc table is ".$userID."</h5>";
+    //   }
+    // }
+
+
+    if ($result1 = $dbconn->query($query1)) {
+
+      while ($row = $result1->fetch_assoc()) {
+        $field1name = $row["full_name"];
+        $field2name = $row["city"];
+        $field3name = $row["state"];
+        $field4name = $row["zip"];
+        $userID = $row['user_id'];
+
+        echo nl2br("\r\nName: ".$field1name);
+
+        echo nl2br("\r\nState: ".$field3name);
+
+        echo nl2br("\r\nCity: ".$field2name);
+
+        echo nl2br("\r\nZip: ".$field4name);
+        
+        echo nl2br("\r\nUser ID: ".$userID);
+      }
     }
-  }
-  ?>
 
-     </div>
 
-         
+    ?>
+
+
+    <div class="container" id="buttons">
+
       <a href="login.php">Don't wanna be here? LEAVE!</a>
       <a href="register.php">wanna register more information?</a>
     </div>
 
-  </div>
+  </main>
 
 </body>
 
