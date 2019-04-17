845a2900b67031b0882d5eb2eb4392040d814a25
nabilahsan <nabilahsan95@gmail.com>
2019-04-16 22:39:14 -0500

    deleted fuel calculator pages. not needed.
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- fuel-history.php
+++
@@ -1,9 +0,0 @@
-<!-- Pulls in stuff from database 
-to show the user the purcahse -->
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Fuel History</title>
-	<link rel="stylesheet" type="text/css" href="resource/styles.css">
-</head>
