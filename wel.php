cfb74dab2ec038e4738ad0dad04a21b511f27e2d
nabilahsan <nabilahsan95@gmail.com>
2019-04-21 16:46:14 -0500

    progresss
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- wel.php
+++ wel.php
@@ -11,8 +11,7 @@ include_once('db_conn.php');
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Our stylesheet -->
   <link rel="stylesheet" href="resources/style.css">
@@ -26,49 +25,42 @@ include_once('db_conn.php');
   <main class="container">
 
     <?php
-
-    echo "<h1> User " . $_SESSION['user_name']. " with ID". $_SESSION['user_id'] . " logged in.\r\n </h1>";
     $uname = $_SESSION['user_name'];
-    $userID = $_SESSION['user_id'];
-    $fuel_userid = $_SESSION['cust_user_id'];
 
 
     $query1 = "SELECT * FROM log WHERE user_name='$uname'";
-    $query2 = "SELECT cust_user_id FROM fuelcalc WHERE user_id = '$fuel_userid'";
-
-    if ($result2 = $dbconn->query($query2)){
-      while ($row = $result2->fetch_assoc()){
-        $fuel_userid = $row["cust_user_id"];
-        echo "<h5> User id from fuelcalc table is ".$fuel_userid."</h5>";
-      }
-    } else {
-      echo "\r\nUser does not exist";
-    }
 
 
+    // Information from Login Table
     if ($result1 = $dbconn->query($query1)) {
 
       while ($row = $result1->fetch_assoc()) {
-        $field1name = $row["full_name"];
-        $field2name = $row["city"];
-        $field3name = $row["state"];
-        $field4name = $row["zip"];
+        $fullName = $row["full_name"];
+        $cityName = $row["city"];
+        $stateName = $row["state"];
+        $zipCode = $row["zip"];
+        $userID = $row["user_id"];
 
-        // User ID from Log
-        $userID = $row['user_id'];
-
-        echo nl2br("\r\nName: " . $field1name);
-
-        echo nl2br("\r\nState: " . $field3name);
-
-        echo nl2br("\r\nCity: " . $field2name);
-
-        echo nl2br("\r\nZip: " . $field4name);
-
+        echo nl2br("\r\nName: " . $fullName);
+        echo nl2br("\r\nCity: " . $cityName);
+        echo nl2br("\r\nState: " . $stateName);
+        echo nl2br("\r\nZip: " . $zipCode);
         echo nl2br("\r\nUser ID_Login: " . $userID);
       }
     }
 
+    // Trying to display fuel calc info JUST for logged user
+
+    $query2 = "SELECT cust_user_id FROM fuelcalc where cust_username = $uname";
+    $result2 = $dbconn->query($query2);
+
+    if ($result2->num_rows > 0) {
+      while ($row = $result2->fetch_assoc()) {
+        echo nl2br("\r\n User ID from fuel table is: " . $row["cust_user_id"]);
+      }
+    } else {
+      echo "<h2> NOOOOOOOOOOOOOOOOOOOOOOOO </h2>";
+    }
 
     ?>
 
@@ -86,22 +78,19 @@ include_once('db_conn.php');
 
       <div class="container" id="main">
 
-
-        <h1 id="welcome">Hello, User.</h1>
-
-
         <form method="post" action="#" class="form-group">
           <div class="container" style="margin-top: 40px">
 
             <h2>Enter information below</h2>
 
             <label>Requested Number of Gallons</label>
-            <input class="form-control" type="number" id="numGallons" name="numGallons" required
-              placeholder="This must be a number" min="1" max="100000">
+            <input class="form-control" type="number" id="numGallons" name="numGallons" required placeholder="This must be a number" min="1" max="100000">
             <br><br>
 
             <div id="date_value" name="date_value">
               <h3> For Delivery Date</h3>
+
+              <!-- Assign values to Month -->
               <label>Choose the month:</label>
               <?php
               $MonthArray = array("1" => "January", "2" => "February", "3" => "March", "4" => "April", "5" => "May", "6" => "June", "7" => "July", "8" => "August", "9" => "September", "10" => "October", "11" => "November", "12" => "December");
@@ -117,13 +106,11 @@ include_once('db_conn.php');
               </select>
 
               <label>Choose the Day of the month:</label>
-              <input name="chooseDay" id="chooseDay" type="text" class="form-control"
-                placeholder="Enter the day as a number, like 14." required>
+              <input name="chooseDay" id="chooseDay" type="text" class="form-control" placeholder="Enter the day as a number, like 14." required>
 
               <!-- Enter year -->
               <label>Enter the year:</label>
-              <input name="chooseYear" id="chooseYear" type="text" class="form-control" placeholder="2019" min="2019"
-                required>
+              <input name="chooseYear" id="chooseYear" type="text" class="form-control" placeholder="2019" min="2019" required>
 
             </div>
 
@@ -148,11 +135,12 @@ include_once('db_conn.php');
             <!-- Should be PHP script -->
             <!-- With database calls -->
 
-          <h2>Fuel History</h2>
-          <?php
-          $sql = "SELECT OrderID, numGallons, c_month, c_day, c_year, price_per_gallon, trans_cost, total_price FROM fuelcalc";
-          $result = $conn->query($sql);
-          echo "<table border='1'>
+            <h2>Fuel History</h2>
+
+            <?php
+            $sql = "SELECT OrderID, numGallons, c_month, c_day, c_year, price_per_gallon, trans_cost, total_price FROM fuelcalc";
+            $result = $conn->query($sql);
+            echo "<table border='1'>
             <tr>
               <th>OrderID</th>
               <th>numGallons</th>
@@ -164,26 +152,26 @@ include_once('db_conn.php');
               <th>total_price</th>
             </tr>";
 
-          if ($result->num_rows > 0) {
-            while ($row =  $result->fetch_assoc()) {
-              echo "<tr>";
-              echo "<td>" . $row['OrderID'] . "</td>";
-              echo "<td>" . $row['numGallons'] . "</td>";
-              echo "<td>" . $row['c_month'] . "</td>";
-              echo "<td>" . $row['c_day'] . "</td>";
-              echo "<td>" . $row['c_year'] . "</td>";
-              echo "<td>" . $row['price_per_gallon'] . "</td>";
-              echo "<td>" . $row['trans_cost']."</td>";
-              echo "<td>" . $row['total_price']."</td>";
-              echo "</tr>";
+            if ($result->num_rows > 0) {
+              while ($row =  $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['OrderID'] . "</td>";
+                echo "<td>" . $row['numGallons'] . "</td>";
+                echo "<td>" . $row['c_month'] . "</td>";
+                echo "<td>" . $row['c_day'] . "</td>";
+                echo "<td>" . $row['c_year'] . "</td>";
+                echo "<td>" . $row['price_per_gallon'] . "</td>";
+                echo "<td>" . $row['trans_cost'] . "</td>";
+                echo "<td>" . $row['total_price'] . "</td>";
+                echo "</tr>";
+              }
             }
-          }
 
-          echo "</table>";
-          $conn->close();
-          ?>
+            echo "</table>";
+            $conn->close();
+            ?>
 
-      </div>
+          </div>
 
 
     </section>
